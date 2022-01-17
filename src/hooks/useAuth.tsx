026f3fdbbae8a5dc065a6feb8748b1dd5c554d4d
Gabriel Souza <gabrielss_@outlook.com>
2022-01-17 18:11:36 -0300

    feat: apple sign in function done

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,4 +1,6 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
+import * as AppleAuthentication from "expo-apple-authentication";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface User {
   id: string;
@@ -13,20 +15,44 @@ interface AuthProviderProps {
 
 interface AuthContextData {
   user: User;
+  signInWithApple(): Promise<void>;
 }
 
 const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-  const user = {
-    id: "1234567",
-    name: "Gabriel",
-    email: "gabrielss@outlook.com",
-    photo: "/fdsafdsa",
-  };
+  const [user, setUser] = useState<User>({} as User);
+  const userStorageKey = "@mymovies:user";
+
+  async function signInWithApple() {
+    try {
+      const credential = await AppleAuthentication.signInAsync({
+        requestedScopes: [
+          AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+          AppleAuthentication.AppleAuthenticationScope.EMAIL,
+        ],
+      });
+
+      if (credential) {
+        const userLogged = {
+          id: String(credential.user),
+          email: credential.email!,
+          name: credential.fullName!.givenName!,
+        };
+
+        setUser(userLogged);
+
+        await AsyncStorage.setItem(userStorageKey, JSON.stringify(userLogged));
+
+        console.log(user);
+      }
+    } catch (error) {
+      throw new Error(error as string);
+    }
+  }
 
   return (
-    <AuthContext.Provider value={{ user: user }}>
+    <AuthContext.Provider value={{ user: user, signInWithApple }}>
       {children}
     </AuthContext.Provider>
   );
